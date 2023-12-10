0e5422ec6849446d069a8a7ee9fd4223ee801b73
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2023-12-11 02:53:23 +0300

    correct the problem with query
    cannot fetch the selected DOM. Changed the selectors and match it in the java

--- script.js
+++ script.js
@@ -17,17 +17,22 @@ function operate(operator, num1, num2) {
   return add(num1, num2);
 }
 const result = document.querySelector(".result");
-const btn1 = document.querySelector(".1");
-const btn2 = document.querySelector(".2");
-const btn3 = document.querySelector(".3");
-const btn4 = document.querySelector(".4");
-const btn5 = document.querySelector(".5");
-const btn6 = document.querySelector(".6");
-const btn7 = document.querySelector(".7");
-const btn8 = document.querySelector(".8");
-const btn9 = document.querySelector(".9");
-const btnPlus = document.querySelector(".+");
-const btnMinus = document.querySelector(".-");
-const btnMulti = document.querySelector(".*");
-const btndivide = document.querySelector("./");
-const btnMinus2 = document.querySelector(".-");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
+const btn4 = document.querySelector(".btn4");
+const btn5 = document.querySelector(".btn5");
+const btn6 = document.querySelector(".btn6");
+const btn7 = document.querySelector(".btn7");
+const btn8 = document.querySelector(".btn8");
+const btn9 = document.querySelector(".btn9");
+const btnPlus = document.querySelector(".btn-plus");
+const btnMinus = document.querySelector(".btn-minus");
+const btnMulti = document.querySelector(".btn-multiply");
+const btndivide = document.querySelector(".btn-divide");
+const btnMinus2 = document.querySelector(".btn-minus");
+const equal = document.querySelector(".btn-equal");
+
+btn1.addEventListener("click", () => {
+  result.textContent = "1";
+});
