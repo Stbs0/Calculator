9bf9692a27137773de98f1f814deb3a80a448999
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2023-12-11 02:18:26 +0300

    define the btns in java and tweak css
    ready to minuplite the dom with javascript and centred the result

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ let num2 = 0;
 function operate(operator, num1, num2) {
   return add(num1, num2);
 }
+const result = document.querySelector(".result");
+const btn1 = document.querySelector(".1");
+const btn2 = document.querySelector(".2");
+const btn3 = document.querySelector(".3");
+const btn4 = document.querySelector(".4");
+const btn5 = document.querySelector(".5");
+const btn6 = document.querySelector(".6");
+const btn7 = document.querySelector(".7");
+const btn8 = document.querySelector(".8");
+const btn9 = document.querySelector(".9");
+const btnPlus = document.querySelector(".+");
+const btnMinus = document.querySelector(".-");
+const btnMulti = document.querySelector(".*");
+const btndivide = document.querySelector("./");
+const btnMinus2 = document.querySelector(".-");
