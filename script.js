9f244f9bff5b15fc2adf077ad4c6dd2eba87981e
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2023-12-11 00:18:30 +0300

    Add bunch of codes
    added the layout of the calculator buttons and the backbone functions in the javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+let num1 = 0;
+let operator = 0;
+let num2 = 0;
