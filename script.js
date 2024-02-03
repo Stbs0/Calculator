fdf17dad5d197690fdd852d6c0d9e42187da8bcb
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-02-04 02:13:17 +0300

    still the bug is there

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-const result = document.querySelector(".result");
-const numberBtn = document.querySelectorAll(".number");
-const operatorBtns = document.querySelectorAll(".op");
+const result = document.querySelector('.result');
+const numberBtn = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.op');
 
-const clear = document.querySelector(".clear");
-const equal = document.querySelector(".equal");
+const clear = document.querySelector('.clear');
+const equal = document.querySelector('.equal');
 let num1 = 0;
 let num2 = 0;
-let operator = "";
-let value = "";
+let operator = '';
+let displayValue = '';
+let opResult = '';
 
 function operate(operator) {
   switch (operator) {
-    case "+":
+    case '+':
       return num1 + num2;
-    case "-":
+    case '-':
       return num1 - num2;
-    case "*":
+    case '*':
       return num1 * num2;
-    case "/":
+    case '/':
       return num1 / num2;
 
     default:
@@ -26,44 +27,46 @@ function operate(operator) {
 }
 function populateDisplay() {
   numberBtn.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      // result.textContent = result.textContent.concat(`${e.target.textContent}`);
-      if (operator === "") {
-        value = "";
-        result.textContent = result.textContent.concat(
-          `${e.target.textContent}`
-        );
-        num1 = +result.textContent;
+    btn.addEventListener('click', (e) => {
+      result.textContent = '';
+      displayValue = displayValue.concat(`${e.target.textContent}`);
+      result.textContent = displayValue;
+      if (operator === '') {
+        num1 = +displayValue;
       }
-      if (operator !== "") {
-        result.textContent += result.textContent.concat(
-          `${e.target.textContent}`
-        );
-        num2 = +result.textContent;
+
+      if (operator !== '') {
+        num2 = +displayValue;
       }
     });
   });
 
   operatorBtns.forEach((op) => {
-    op.addEventListener("click", (e) => {
+    op.addEventListener('click', (e) => {
+      displayValue = '';
       operator = e.target.textContent;
       if (num2 !== 0) {
         num1 = operate(operator);
+
         num2 = 0;
+        operator = '';
         result.textContent = num1;
       }
     });
   });
-  equal.addEventListener("click", (e) => {
+  equal.addEventListener('click', (e) => {
     num1 = operate(operator);
     num2 = 0;
+    operator = '';
+    displayValue = '';
     result.textContent = num1;
   });
-  clear.addEventListener("click", () => {
+  clear.addEventListener('click', () => {
     num1 = 0;
     num2 = 0;
-    operator = "";
-    result.textContent = "";
+    operator = '';
+    displayValue = '';
+    result.textContent = '';
   });
 }
 populateDisplay();
